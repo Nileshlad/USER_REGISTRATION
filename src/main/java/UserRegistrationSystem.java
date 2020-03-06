7927440f410ad721f5c507164d38a8355e4cfef7
Nileshlad <ladnilesh1111@gmail.com>
2020-03-06 18:57:17 +0530

    Added: To check first name valid

--- src/main/java/UserRegistrationSystem.java
+++ src/main/java/UserRegistrationSystem.java
@@ -1,6 +1,18 @@
+import java.util.regex.Pattern;
+
 public class UserRegistrationSystem {
+
+    //DECLARE PATTERN
+    String firstNamePattern="^[A-Z][a-z]{2,}$";
+
+    //WELCOME MESSAGE
     public static void main (String args[]){
         System.out.println("WELL COME USER REGISTRATION SYSTEM");
+    }
+
+    //  METHOD TO VALIDATE FIRST NAME
+    public boolean validateFirstName(String firstName) {
+        return (Pattern.matches(firstNamePattern,firstName));
     }
 
 }
--- src/test/java/UserRegistrationSytemTest.java
+++ src/test/java/UserRegistrationSytemTest.java
@@ -1,2 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserRegistrationSytemTest {
+    UserRegistrationSystem userregistrationproblem = new UserRegistrationSystem();
+
+    //TEST TO CHECK  FIRST NAME VALID
+    @Test
+    public void givenFirstName_whenValid_thenReturn() {
+        String name="Nilesh";
+        boolean firstName = userregistrationproblem.validateFirstName(name);
+        Assert.assertTrue(firstName);
+    }
+
+    //TEST TO CHECK FIRST NAME INVALID
+    @Test
+    public void givenFirstName_whenInvalid_thenReturn() {
+        String name="nilesh";
+        boolean firstName = userregistrationproblem.validateFirstName(name);
+        Assert.assertFalse(firstName);
+    }
+
 }
